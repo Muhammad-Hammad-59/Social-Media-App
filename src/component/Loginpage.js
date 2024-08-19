4d146bb456415156a5ca1a3fa8fa57279b6432a1
Muhammad Hammad <muhammadhammad010100@gmail.com>
2024-08-18 23:36:20 -0700

    fisrt commit

---
+++ src/component/Loginpage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loginpage = () => {
+  return (
+    <div>
+      <h1 className='text-2xl text-fuchsia-500'>login page</h1>
+    </div>
+  );
+}
+
+export default Loginpage;
